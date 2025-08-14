e8be0f3df34cc6d5a8649f6ac2c2a633ec3e9bb6
MonHubAgri <monhubagri@gmail.com>
2025-08-14 14:11:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+(async () => {
+  const url = 'https://www.pleinchamp.com/actualites';
+  try {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    const articles = [];
+
+    $('.card-article').each((i, el) => {
+      articles.push({
+        titre: $(el).find('h2').text().trim(),
+        lien: `https://www.pleinchamp.com${$(el).find('a').attr('href')}`,
+        description: $(el).find('p').text().trim(),
+        image: $(el).find('img').attr('src'),
+        date: $(el).find('time').attr('datetime')
+      });
+    });
+
+    console.log(articles);
+  } catch (err) {
+    console.error('Erreur :', err.message);
+  }
+})();
